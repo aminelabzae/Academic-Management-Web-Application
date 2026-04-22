c9a6019af81a27c5827ef15a9783b30ff56a94b4
Amine Labzae <aminelabzae651@gmail.com>
2026-04-22 16:44:43 +0100

    chore: remove unnecessary files and update .gitignore

--- storage/framework/views/3a431ca80150f06e0fa67a1672f9e567.php
+++
@@ -1,4 +0,0 @@
-<div <?php echo e($attributes->merge(['class' => "h-0 w-full relative"])); ?>>
-    <div class="absolute top-[-1px] left-0 right-0 bottom-0 border-t border-dashed border-neutral-300 dark:border-white/[9%]"></div>
-</div>
-<?php /**PATH C:\Users\aminelabzae\OneDrive\Documents\Programmation\2em-annee\laravel\emploi-du-temps (8)\emploi-du-temps\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/separator.blade.php ENDPATH**/ ?>
--- storage/framework/views/5e60c06b9449af4205b3ddced1ad862f.php
+++
@@ -1,8 +0,0 @@
-<section
-    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
-
->
-    <?php echo e($slot); ?>
-
-</section>
-<?php /**PATH C:\Users\aminelabzae\OneDrive\Documents\Programmation\2em-annee\laravel\emploi-du-temps (8)\emploi-du-temps\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
